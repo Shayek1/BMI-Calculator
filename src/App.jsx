0505b5e599a58b8f4249dfac055030e1ca27bee1
Shayek Ahmed <shayek.a@outlook.com>
2025-11-22 17:10:57 +0000

    built the calculation process

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,29 @@
-import React from "react";
+import React, {useState} from "react";
 
 const App = () => {
+
+    const [height, setHeight] = useState("");
+    const [weight, setWeight] = useState("");
+    const [results, setResults] = useState("");
+
+    const bmiFormula = () => {
+        if (!weight || !height) {
+            setResults("Enter your height and weight");
+        }
+
+        const bmiCalc = (weight / ((height/100) * (height/100)))
+        const bmiWholeNumber = bmiCalc.toFixed(1);
+
+        let bmiDivision = "";
+        if (0 < bmiWholeNumber < 18.5) bmiDivision = "UNDERWEIGHT";
+        else if (bmiDivision < 25) bmiDivision = "NORMAL WEIGHT";
+        else if (bmiDivision < 30) bmiDivision = "OVERWEIGHT";
+        else if (bmiDivision < 40) bmiDivision = "OBESE";
+        else bmiDivision = "MORBIDLY OBESE";
+
+        setResults(`Your BMI is ${bmiWholeNumber} : You fall in the ${bmiDivision} category`);
+    }
+
     return <div>
 
         <div className="container">
@@ -14,7 +37,7 @@ const App = () => {
             <label htmlFor="Weight">Weight (kg)</label>
             <input type="number" placeholder="Example: 50" id="Weight"/>
 
-            <button>Calculate BMI</button>
+            <button onClick={bmiFormula}>Calculate BMI</button>
 
             <div className="results">Testing</div>
 
