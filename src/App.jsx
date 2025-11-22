fd6a87aab0c1c008bf02cd6fc90fec692452f115
Shayek Ahmed <shayek.a@outlook.com>
2025-11-22 14:15:54 +0000

    Add height and weight inputs

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,23 @@ import React from "react";
 
 const App = () => {
     return <div>
-        BMI Calculator
+
+        <div className="container">
+        <h1>BMI Calculator</h1>
+
+            {/* Height */}
+            <label htmlFor="Height"> Height (cm)</label>
+            <input type="number" placeholder="Example: 150" id="Height"/>
+
+            {/* Weight */}
+            <label htmlFor="Weight">Weight (kg)</label>
+            <input type="number" placeholder="Example: 50" id="Weight"/>
+
+            <Button>Calculate BMI</Button>
+
+
+
+        </div>
     </div>;
     };
 
